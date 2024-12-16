fe5680b446e0c6d78cc4899e0e483cf0d0c94b57
Karsten <karsten.walz.jensen@gmail.com>
2024-12-16 14:27:47 +0100

    more plots fixed, need to look at linesearcher

--- classes/calculators/lennardjones.py
+++ classes/calculators/lennardjones.py
@@ -22,26 +22,25 @@ class LennardJones(Calculator):
         return 4.0*self.eps0*((self.sigma/r)**12-(self.sigma/r)**6)
 
 
-class LJGauss(LennardJones):
-    def __init__(self, pbc=False, pbc_handler=None, 
+class LJGauss(Calculator):
+    def __init__(self, 
                  gauss_sigma2=0.02,
                  gauss_eps=1.0,
-                 r0=1.0,
-                 lennard_eps=1.0, 
-                 lennard_sigma=1.0
+                 r0=1.7
                  ):
         self.gauss_sigma2 = gauss_sigma2
         self.gauss_eps = gauss_eps
         self.r0 = r0
-        super().__init__(pbc, pbc_handler, lennard_eps, lennard_sigma)
     
     def _V(self, r):
-        lennard_term = super()._V(r)
+        lennard_term = (1.0/r)**12-2.0*(1.0/r)**6
         gauss_term = self.gauss_eps*np.exp(-(r-self.r0)**2/(2.0*self.gauss_sigma2))
         return lennard_term - gauss_term
     
     def _dV_dr(self, r):
-        lennard_term = super()._dV_dr(r)
+        t1 = 6.0*(1.0**6)/(r**7)
+        t2 = 2.0*12.0*(1.0**12)/(r**13)
+        lennard_term = (t1 - t2)
         gauss_term = self.gauss_eps/self.gauss_sigma2*np.exp(-(r-self.r0)**2/(2.0*self.gauss_sigma2))*(r-self.r0)
         return lennard_term + gauss_term
 
